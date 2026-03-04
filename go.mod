fc87846ecee6e5594b48bab88f90936bb1ee095f
lberrymage <logan@lberrymage.dev>
2026-03-04 02:42:08 +0000

    Add initial gateway implementation
    Closes #3

---
+++ go.mod
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: © 2026 Logan Magee
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+module golang.accrescent.app/console-grpc-gateway
+
+go 1.26.0
+
+require (
+	buf.build/gen/go/accrescent/console-api/grpc-ecosystem/gateway/v2 v2.28.0-20260219033837-64def0185a8e.1
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.28.0
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260226221140-a57be14db171
+	google.golang.org/grpc v1.79.1
+)
+
+require (
+	buf.build/gen/go/accrescent/console-api/grpc/go v1.6.1-20260219033837-64def0185a8e.1 // indirect
+	buf.build/gen/go/accrescent/console-api/protocolbuffers/go v1.36.11-20260219033837-64def0185a8e.1 // indirect
+	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.36.11-20260209202127-80ab13bee0bf.1 // indirect
+	buf.build/gen/go/grpc-ecosystem/grpc-gateway/protocolbuffers/go v1.36.11-20260102203250-6467306b4f62.1 // indirect
+	cloud.google.com/go/longrunning v0.8.0 // indirect
+	golang.org/x/net v0.51.0 // indirect
+	golang.org/x/sys v0.41.0 // indirect
+	golang.org/x/text v0.34.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20260226221140-a57be14db171 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
+)
---
+++ main.go
@@ -0,0 +1,88 @@
+// SPDX-FileCopyrightText: © 2026 Logan Magee
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	gw "buf.build/gen/go/accrescent/console-api/grpc-ecosystem/gateway/v2/accrescent/console/v1alpha1/consolev1alpha1gateway"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	_ "google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/grpclog"
+)
+
+func main() {
+	cfg := &config{}
+	flag.StringVar(
+		&cfg.backendEndpoint,
+		"grpc-server-endpoint",
+		"localhost:50051",
+		"gRPC server endpoint",
+	)
+	flag.UintVar(
+		&cfg.listenPort,
+		"listen-port",
+		8080,
+		"port to listen on",
+	)
+	flag.Parse()
+
+	if err := run(cfg); err != nil {
+		grpclog.Fatal(err)
+	}
+}
+
+type config struct {
+	backendEndpoint string
+	listenPort      uint
+}
+
+func run(config *config) error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	conn, err := grpc.NewClient(config.backendEndpoint, opts...)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if cerr := conn.Close(); err != nil {
+			grpclog.Errorf("Failed to close conn to %s: %v", config.backendEndpoint, cerr)
+		}
+	}()
+
+	mux := runtime.NewServeMux()
+	if err := gw.RegisterAppDraftServiceHandler(ctx, mux, conn); err != nil {
+		return err
+	}
+	if err := gw.RegisterAppEditServiceHandler(ctx, mux, conn); err != nil {
+		return err
+	}
+	if err := gw.RegisterAppServiceHandler(ctx, mux, conn); err != nil {
+		return err
+	}
+	if err := gw.RegisterOrganizationServiceHandler(ctx, mux, conn); err != nil {
+		return err
+	}
+	if err := gw.RegisterReviewServiceHandler(ctx, mux, conn); err != nil {
+		return err
+	}
+	if err := gw.RegisterUserServiceHandler(ctx, mux, conn); err != nil {
+		return err
+	}
+
+	listenAddr := fmt.Sprintf(":%d", config.listenPort)
+	log.Printf("Starting gateway on %s", listenAddr)
+
+	return http.ListenAndServe(listenAddr, mux)
+}
